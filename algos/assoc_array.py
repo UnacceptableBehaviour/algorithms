c4abad76bae444be3aee34ea859c0c140ad8c501
Simon <simon@ferdiesfoodlab.co.uk>
2020-04-18 19:42:50 +0100

    insert and retrieve w collisions

--- algos/assoc_array.py
+++ algos/assoc_array.py
@@ -11,7 +11,7 @@ import math
 # Using: hash table or a search tree
 # 
 # insert: value at key location 
-# assign: replace a value @ key
+# assign_or_append: replace a value @ key
 # delete: remove a key value pair
 # lookup: serach for and retrieve value based on key
 
@@ -22,14 +22,17 @@ class Collision_Chain:
     def __init__(self, key, value):
         self.collisions = [(key, value)]
 
-    # assign: replace a value @ key
-    def assign(self, key, value):
+    # assign_or_append: replace a value @ key
+    def assign_or_append(self, key, value=None):
+        if value == None and key.__class__ == tuple:    
+            key, value = key                    # allow passing in a tuple instead of key, value
+            
         # if i in collision: overwrite          < search - scan
         # else: append kv pair                  < allocate mem        
         for i,kv_pair in enumerate(self.collisions):
             if kv_pair[Collision_Chain.KEY] == key:
                 kv_pair[Collision_Chain.VALUE] = value
-                return                          # found and assigned
+                return                          # found and assign_or_appended
         
         self.collisions.append((key, value))    # not found append
                         
@@ -65,31 +68,78 @@ class Collision_Chain:
 class Assoc_Array:
     INITIAL_BUCKETS = 11
         
-    def __init__(self): # ('lamb chop', 'butcher') ('smoothie', 'drink') ('chocolate', 'confectionary') ('jelly beans', 'confectionary')
+    def __init__(self): 
         self.entries_n = 0
         self.size_mk = Assoc_Array.INITIAL_BUCKETS
         self.store = [None] * self.size_mk
 
 
     # insert: value at key location 
-    def insert(key, value):
+    def insert(self, key, value):
         success = False
+        index = self.aa_hash(key)
+        
+        if self.store[index] == None:      # most likely case if well designed
+            self.store[index] = (key, value)
+            
+        else:                       # overwrite or collision(with a tuple or a chain)
+            if self.store[index].__class__ == tuple and self.store[index][0] == key:
+                self.store[index][1] == value
+                
+            elif self.store[index].__class__ == Collision_Chain:
+                self.store[index].assign_or_append(key, value)
+                
+            else:
+                chain = Collision_Chain(key, value)         # store collision
+                chain.assign_or_append(self.store[index])   # add resident value to chain
+                self.store[index] = chain                   # store chain in place
+
+        success = True
+        self.entries_n += 1
+        
         return success
     
     # assign: replace a value @ key
-    def assign(key, value):
-        success = False
-        return success
+    # insert does this
+    # def assign(key, value):
+    #     success = False
+    #     return success
             
     # delete: remove a key value pair
-    def delete(key):
+    def delete(self, key):
         success = False
+        
+        success = True
+        self.entries_n -= 1
+        
         return success
     
     # lookup: serach for and retrieve value based on key
-    def get(key):
-        success = False
-        return success
+    def get(self, key):        
+        index = self.aa_hash(key)
+        
+        if self.store[index].__class__ == tuple:
+            return self.store[index][1]
+            
+        elif self.store[index].__class__ == Collision_Chain:
+            return self.store[index].get(key)
+            
+        else:        
+            raise f"Unknown Error trying to retrive key '{key}'"
+
+    def aa_hash(self, key):        
+        hash_val = 0
+        
+        msg = f"hashing: {key}"
+        for byte in bytes(key,'utf8'):
+            hash_val = hash_val + byte
+        
+        hash_val = hash_val % self.size_mk
+
+        print(f"{msg} - hash: {hash_val}")
+                    
+        return hash_val
+        
         
     def __str__(self):
         return ("__str__")
@@ -104,35 +154,35 @@ class Assoc_Array:
     def __len__(self):
         return self.size_mk
     
-class Entry:        
-    size_mk = Assoc_Array.INITIAL_BUCKETS
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value            
-        self.hash = None
-        
-
-    def __str__(self):
-        return ("__str__")
-
-    def __repr__(self):
-        return ("__repr__")
-    
-    def __hash__(self):
-        # hash calculated
-        if self.hash: return self.hash
-        
-        hash_val = 0
-        print(f"hashing: {self.key}")
-        for byte in bytearray(self.key):
-            hash_val = hash_val + byte
-            print(byte)
-        
-        self.hash = hash_val % Entry.size_mk
-        print(f"hash: {self.hash}")
-                    
-        return self.hash
+# class Entry:        
+#     size_mk = Assoc_Array.INITIAL_BUCKETS
+# 
+#     def __init__(self, key, value):
+#         self.key = key
+#         self.value = value            
+#         self.hash = None
+#         
+# 
+#     def __str__(self):
+#         return ("__str__")
+# 
+#     def __repr__(self):
+#         return ("__repr__")
+#     
+#     def __hash__(self):
+#         # hash calculated
+#         if self.hash: return self.hash
+#         
+#         hash_val = 0
+#         print(f"hashing: {self.key}")
+#         for byte in bytearray(self.key):
+#             hash_val = hash_val + byte
+#             print(byte)
+#         
+#         self.hash = hash_val % Entry.size_mk
+#         print(f"hash: {self.hash}")
+#                     
+#         return self.hash
         
 
 if __name__ == '__main__':
@@ -144,9 +194,9 @@ if __name__ == '__main__':
     print(collisions.delete('banana'))
     print(collisions.get('banana'))
     
-    collisions.assign('lamb chop', 'butcher')
-    collisions.assign('cabbage', 'veg')
-    collisions.assign('smoothie', 'drink')    
+    collisions.assign_or_append('lamb chop', 'butcher')
+    collisions.assign_or_append('cabbage', 'veg')
+    collisions.assign_or_append('smoothie', 'drink')    
     
     pprint(collisions)
     
@@ -155,8 +205,8 @@ if __name__ == '__main__':
     
     pprint(collisions)
     
-    collisions.assign('chocolate', 'confectionary')
-    collisions.assign('jelly beans', 'confectionary')
+    collisions.assign_or_append('chocolate', 'confectionary')
+    collisions.assign_or_append('jelly beans', 'confectionary')
     pprint(collisions)
     
     print(f"get('banana') - {collisions.get('banana')}")
@@ -168,6 +218,71 @@ if __name__ == '__main__':
     
     pprint(aa)
     
+    aa.insert('lamb chop', 'butcher')    
+    pprint(aa)
+    
+    aa.insert('lamb chop', 'organic butcher')    
+    pprint(aa)
+    
+    aa.insert('cabbage', 'veg')    
+    pprint(aa)
+    
+    aa.insert('chocolate', 'confectionary')    
+    pprint(aa)
+    
+    aa.insert('jelly beans', 'confectionary')
+    pprint(aa)
+
+    aa.insert('smoked garlic','herbs n spices')
+    pprint(aa)
+    aa.insert('bay leaf','herbs n spices')
+    pprint(aa)
+    aa.insert('chicken makhani','world foods')
+    pprint(aa)
+    aa.insert('sweet paprika','herbs n spices')
+    pprint(aa)
+    aa.insert('guinea fowl','poultry')
+    pprint(aa)
+    aa.insert('kiwi fruit','fruit')
+    pprint(aa)
+    aa.insert('tomato paste','herbs n spices')
+    pprint(aa)
+    aa.insert('cocktail beetroot','veg')
+    pprint(aa)
+    aa.insert('lamb shoulder','butcher')
+    pprint(aa)
+    
+    find = 'jelly beans'
+    print(f"{find} = ", aa.get(find))
+    find = 'chocolate'
+    print(f"{find} = ", aa.get(find))
+    find = 'cabbage'
+    print(f"{find} = ", aa.get(find))
+    find = 'chicken makhani'
+    print(f"{find} = ", aa.get(find))
+    find = 'kiwi fruit'
+    print(f"{find} = ", aa.get(find))
+    find = 'lamb chop'
+    print(f"{find} = ", aa.get(find))
+
+    
     sys.exit(0)   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - EXIT < <
 
+    # for b in bytearray(b'suasage'):
+    #     print(b)    
+    # key = 'cucumber'
+    # for n in key: #.iterbytes():
+    #     print(n)    
+    # ba = bytearray(key,'utf8')    
+    # for b in bytearray(key,'utf8'):
+    #     print(b)
+
+    # tpl = (5, 99)
+    # print(type(tpl.__class__))
+    # print(type(tpl))
+    # print(tpl.__class__ == tuple)
+    # key, value = tpl
+    # print(key, value)
+    # 
+    # print(len(aa))
 
