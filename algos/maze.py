244758e792d633ce45b94fa0cd0feacd6e34e774
Simon <simon@ferdiesfoodlab.co.uk>
2020-11-24 23:11:09 +0000

    Maze class to test BFS & DFS

---
+++ algos/maze.py
@@ -0,0 +1,247 @@
+#! /usr/bin/env python
+# 3.7
+
+from collections import deque
+import random
+from pprint import pprint
+
+class MazeNode:
+	def __init__(self, name, x, y, u=None,d=None,l=None,r=None):
+		self.name = name
+		self.u = u
+		self.d = d
+		self.l = l
+		self.r = r
+		self.sides = 4 - len([i for i in [u,d,l,r] if i]) # None = wall
+		self.x = x  	# dbg
+		self.y = y  	# dbg
+		
+	def __repr__(self):					# so networkx displays meaningful name on the node!
+		return self.name
+	
+	def add_side(self):	
+		path_pairs = {self.u:self.u.d,
+					  self.d:self.d.u,
+					  self.l:self.l.r,
+					  self.r:self.r.l}		
+		try:
+			del(path_pairs[None])
+		except:
+			pass
+		
+		print("path_pairs: --S")
+		for k in path_pairs.keys():
+			print(path_pairs[k].__class__)
+		
+		wall = random.choice(list(path_pairs.keys()))
+		print(f"wall: {wall.__class__} {wall.x},{wall.y}")
+		path_pairs[wall] = None		FIX THIS
+		wall = None
+		print(f"path_pairs len:{len(path_pairs)}")
+		self.sides += 1
+		print("path_pairs: --E")
+		for k in path_pairs.keys():
+			print(path_pairs[k].__class__)
+
+		return self.sides
+
+
+class Graph:
+	def __init__(self):
+		self.adj = {}
+
+	def add_edge(self, u, v):
+		#if self.adj[u] is None:
+		if u not in self.adj:
+			self.adj[u] = []
+			
+		if v not in self.adj[u]:
+			self.adj[u].append(v)
+	
+	def __repr__(self):
+		node_str = f'{self.__class__.__name__}          nodes - adjacency lists\n'
+		for n in self.adj.keys():
+			node_str += f"{n} ".rjust(22)
+			node_str += ','.join([item.name for item in self.adj[n]])
+			node_str += "\n"
+		return node_str
+
+
+class Maze:
+	def __init__(self, x=10, y=10):
+		self.cell_size = 3
+		self.mz = []
+		self.mz_art = []
+		self.x = x
+		self.y = y
+		for yy in range(self.y):
+			self.mz.append([])
+			for xx in range(self.x):				
+				self.mz[yy].append(MazeNode(xx,yy,f"{str(xx).rjust(2)}{str(yy).rjust(2)}"))
+				#print(xx,yy,"--",f"{str(xx).rjust(2)}{str(yy).rjust(2)}")
+			#pprint(self.mz)
+		
+		#connect nodes
+		for yy in range(self.y):			
+			for xx in range(self.x):
+				node = self.get(xx,yy)
+				node.u = self.get(xx,self.dec_y(yy))
+				node.d = self.get(xx,self.inc_y(yy))
+				node.l = self.get(self.dec_x(xx),yy)
+				node.r = self.get(self.inc_x(xx),yy)
+				node.sides = 0
+				
+		self.create_maze()
+		
+		self.create_art()
+
+	def create_maze(self):
+		'''
+		randomly pick a square and add a side
+		if adding one more side creates a closed box remove it from target list
+		repeat until no targets left
+		'''
+		flat_maze = [maze_node for row in self.mz for maze_node in row]
+		nodes_left = len(flat_maze)
+		print(f"flat_maze len: {nodes_left}")
+		
+		while nodes_left > 20:
+			target_node = random.randint(0, nodes_left-1)
+			sides = flat_maze[target_node].add_side()
+			if sides >= 2:				
+				del flat_maze[target_node]
+				nodes_left = len(flat_maze)
+				print(f"nodes_left: {nodes_left}")
+		
+	
+	def inc_x(self, x):
+		x += 1
+		return x % self.x
+
+	def dec_x(self, x):
+		x -= 1
+		if x < 0: x = self.x - 1
+		return x
+
+	def inc_y(self, y):
+		y += 1
+		return y % self.y
+
+	def dec_y(self, y):
+		y -= 1
+		if y < 0: y = self.y - 1
+		return y		
+		
+	def create_art(self):
+		# build blank art array
+		print(f"create_art: x:{self.x} y:{self.y}")
+		for yy in range((self.y * (self.cell_size-1))+1):	# cells overlap by 1
+			self.mz_art.append([])
+			for xx in range((self.x * (self.cell_size-1))+1):				
+				self.mz_art[yy].append(' ')
+
+		for yy in range(self.y):
+			for xx in range(self.x):			
+				art = self.get_ascii(xx,yy)
+				self.place_art(xx,yy, art)
+		# x = 0
+		# y = 0
+		# art = self.get_ascii(x,y)
+		# self.place_art(x,y, art)
+		# x = 19
+		# y = 0
+		# art = self.get_ascii(x,y)
+		# self.place_art(x,y, art)
+		# #print
+		# x = 0
+		# y = 9
+		# art = self.get_ascii(x,y)
+		# self.place_art(x,y, art)
+		# x = 19
+		# y = 9
+		# art = self.get_ascii(x,y)
+		# self.place_art(x,y, art)
+		
+		
+	def place_art(self, x, y, art):
+		'''
+		over write ascii art with lists in art
+		x,y co-ord of maze position
+		'''
+		xpos = x * (self.cell_size - 1)
+		ypos = y * (self.cell_size - 1)
+		# print(f"place_art {x}:{xpos},{y}:{ypos}")
+		self.mz_art[ypos][xpos:xpos+len(art[0])] = art[0]
+		self.mz_art[ypos+1][xpos:xpos+len(art[1])] = art[1]
+		self.mz_art[ypos+2][xpos:xpos+len(art[2])] = art[2]
+		
+		
+		
+	
+	def get(self,x,y):
+		return self.mz[y][x]		
+	
+	def get_ascii(self,x,y, dbg=False):
+		node = self.get(x,y)
+		stud = '@'
+		blank = ' '
+		
+		if dbg:
+			cpoint = str(node.sides)
+		else:
+			cpoint = blank
+			
+		cell_art = [[stud, stud, stud],
+					[stud, cpoint, stud],
+					[stud, stud, stud] ]		
+		if node.u: cell_art[0][1] = blank
+		if node.d: cell_art[2][1] = blank
+		if node.l: cell_art[1][0] = blank
+		if node.r: cell_art[1][2] = blank
+		return cell_art
+	
+
+	def __repr__(self):
+		'''
+		cell four corners & 4 ports
+		* *     ***     * *
+		        *       * *
+		* *     * *     * *
+		'''
+		maze_str = f'{self.__class__.__name__}\n'
+		maze_str += '   .123456789.123456789.123456789.123456789.123456789\n'
+		# for yy in range(self.y):
+		# 	for xx in range(self.x):				
+		# 		#maze_str += f"{self.get(xx,yy)} "
+		# 		maze_str += ''.join(self.mz_art[yy])
+		# 	maze_str += '\n'
+		cnt = 0
+		for yy in range(len(self.mz_art)):
+			maze_str += str(cnt).rjust(2)+' '+''.join(self.mz_art[yy])
+			maze_str += '\n'
+			cnt+=1
+
+		return maze_str
+
+
+from pathlib import Path
+import re
+DATAFILE = Path('./scratch/food.txt')
+# to draw graph
+import networkx as nx
+# https://github.com/networkx/networkx
+# https://networkx.org/documentation/stable/tutorial.html
+# https://networkx.org/documentation/stable/reference/drawing.html#drawing
+import matplotlib.pyplot as plt
+
+# TODO try
+# https://graph-tool.skewed.de/
+# https://graph-tool.skewed.de/static/doc/quickstart.html
+
+if __name__ == '__main__':
+
+	m = Maze(20,10)
+	print('\n\ndisplay\n')
+	print(m)
+	
+	
